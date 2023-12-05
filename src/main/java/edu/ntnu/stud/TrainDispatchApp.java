82b3d77c62739f014b499952e962527a6131a755
Adrian <131603611+derzhi@users.noreply.github.com>
2023-12-05 14:43:46 +0100

    added TrainDepartureGroup

--- src/main/java/edu/ntnu/stud/TrainDispatchApp.java
+++ src/main/java/edu/ntnu/stud/TrainDispatchApp.java
@@ -1,13 +1,11 @@
 package edu.ntnu.stud;
 
-import java.time.LocalTime;
-
 /**
  * This is the main class for the train dispatch application.
  */
 public class TrainDispatchApp {
   public static void main(String[] args) {
-    userInterface ui = new userInterface();
+    UserInterface ui = new UserInterface();
     ui.init();
     ui.start();
   }
