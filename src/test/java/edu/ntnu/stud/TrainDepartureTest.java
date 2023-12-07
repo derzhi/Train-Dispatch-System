c859470d6c98fa7f30bdd3e1edce14dc04665bc1
Adrian <131603611+derzhi@users.noreply.github.com>
2023-12-07 19:20:52 +0100

    deleted and added positive and negative tests

--- src/test/java/edu/ntnu/stud/TrainDepartureTest.java
+++ src/test/java/edu/ntnu/stud/TrainDepartureTest.java
@@ -1,5 +1,8 @@
 package edu.ntnu.stud;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalTime;
@@ -7,39 +10,133 @@ import java.time.LocalTime;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TrainDepartureTest {
+  private TrainDeparture td;
 
-  // Positive tests
-  @Test
-  public void testTrainDepartureCreation() {
-    TrainDeparture td = new TrainDeparture(LocalTime.of(15, 30), LocalTime.of(0, 15), "Bergen", "L4", 3123, 5);
+  @Nested
+  @DisplayName("Positive tests for the TrainDeparture class")
+  public class PositiveTrainDepartureTests {
 
-    assertNotNull(td);
-    assertEquals(LocalTime.of(15, 30), td.getDepartureTime());
-    assertEquals(LocalTime.of(0, 15), td.getDelay());
-    assertEquals("Bergen", td.getDestination());
-    assertEquals("L4", td.getLine());
-    assertEquals(3123, td.getTrainNumber());
-    assertEquals(5, td.getTrack());
+    @BeforeEach
+    public void setUp() {
+      td = new TrainDeparture(LocalTime.of(15, 30), LocalTime.of(0, 15), "Bergen", "L4", 3123, -1);
+    }
+
+    @Test
+    public void getDepartureTimeShouldGetCorrectLocalTime() {
+      assertEquals(td.getDepartureTime(), LocalTime.of(15, 30));
+    }
+
+    @Test
+    public void getDelayShouldGetCorrectLocalTime() {
+      assertEquals(td.getDelay(), LocalTime.of(0, 15));
+    }
+
+    @Test
+    public void getDestinationShouldGetCorrectString() {
+      assertEquals(td.getDestination(), "Bergen");
+    }
+
+    @Test
+    public void getLineShouldGetCorrectString() {
+      assertEquals(td.getLine(), "L4");
+    }
+
+    @Test
+    public void getTrainNumberShouldGetCorrectInteger() {
+      assertEquals(td.getTrainNumber(), 3123);
+    }
+
+    @Test
+    public void getTrackNumberShouldGetCorrectInteger() {
+      assertEquals(td.getTrack(), -1);
+    }
+
   }
 
-  @Test
-  public void testToString() {
-    TrainDeparture td = new TrainDeparture(LocalTime.of(15, 30), LocalTime.of(0, 15), "Bergen", "L4", 3123, 5);
-    String expectedString = "TrainDeparture { Departure Time: 15:30, Delay: 00:15, Destination: Bergen, Line: L4, Train Number: 3123, Track: 5 }";
-    assertEquals(expectedString, td.toString());
-  }
+  @Nested
+  @DisplayName("Negative tests for the TrainDeparture class")
+  public class NegativeTrainDepartureTests {
 
-  // Negative tests
-  @Test
-  public void testDefaultDelayTime() {
-    TrainDeparture td = new TrainDeparture(LocalTime.of(15, 30), null, "Bergen", "L4", 3123, 5);
-    assertEquals(LocalTime.of(0, 0), td.getDelay());
-  }
+    @Test
+    public void destinationThrowsOnBlankString() {
+      try {
+        new TrainDeparture(LocalTime.of(15, 30), LocalTime.of(0, 15), "", "L4", 3123, 5);
+        fail("Test failed");
+      } catch (IllegalArgumentException e) {
+        assertEquals("The string for the parameter was a blank string, please retry.", e.getMessage());
+      }
+    }
 
-  @Test
-  public void testDefaultTrackAssignment() {
-    TrainDeparture td = new TrainDeparture(LocalTime.of(15, 30), LocalTime.of(0, 15), "Bergen", "L4", 3123, -5);
-    assertEquals(-1, td.getTrack());
-  }
+    @Test
+    public void lineThrowsOnBlankString() {
+      try {
+        new TrainDeparture(LocalTime.of(15, 30), LocalTime.of(0, 15), "Bergen", "", 3123, 5);
+        fail("Test failed");
+      } catch (IllegalArgumentException e) {
+        assertEquals("The string for the parameter was a blank string, please retry.", e.getMessage());
+      }
+    }
 
+    @Test
+    public void trainNumberThrowsOnNegativeInteger() {
+      try {
+        new TrainDeparture(LocalTime.of(15, 30), LocalTime.of(0, 15), "Bergen", "A4", -3123, 5);
+        fail("Test failed");
+      } catch (IllegalArgumentException e) {
+        assertEquals("The integer for the parameter was negative, please retry.", e.getMessage());
+      }
+    }
+
+    @Test
+    public void trainNumberThrowsOnZeroInteger() {
+      try {
+        new TrainDeparture(LocalTime.of(15, 30), LocalTime.of(0, 15), "Bergen", "A4", 0, 5);
+        fail("Test failed");
+      } catch (IllegalArgumentException e) {
+        assertEquals("The integer for the parameter was negative, please retry.", e.getMessage());
+      }
+    }
+
+    @Test
+    public void trackThrowsOnNegativeIntegerThatIsNotMinusOne() {
+      try {
+        new TrainDeparture(LocalTime.of(15, 30), LocalTime.of(0, 15), "Bergen", "A4", 3253, -2);
+        fail("Test failed");
+      } catch (IllegalArgumentException e) {
+        assertEquals("The integer for the parameter was not over 0 or -1, please retry.", e.getMessage());
+      }
+    }
+
+    @Test
+    public void trackThrowsOnZeroInteger() {
+      try {
+        new TrainDeparture(LocalTime.of(15, 30), LocalTime.of(0, 15), "Bergen", "A4", 3253, 0);
+        fail("Test failed");
+      } catch (IllegalArgumentException e) {
+        assertEquals("The integer for the parameter was not over 0 or -1, please retry.", e.getMessage());
+      }
+    }
+
+    @Test
+    public void setTrackThrowsOnNegativeIntegerThatIsNotMinusOne() {
+      try {
+        TrainDeparture td = new TrainDeparture(LocalTime.of(15, 30), LocalTime.of(0, 15), "Bergen", "A4", 3253, 0);
+        td.setTrack(-2);
+        fail("Test failed");
+      } catch (IllegalArgumentException e) {
+        assertEquals("The integer for the parameter was not over 0 or -1, please retry.", e.getMessage());
+      }
+    }
+
+    @Test
+    public void setTrackThrowsZeroInteger() {
+      try {
+        TrainDeparture td = new TrainDeparture(LocalTime.of(15, 30), LocalTime.of(0, 15), "Bergen", "A4", 3253, 0);
+        td.setTrack(0);
+        fail("Test failed");
+      } catch (IllegalArgumentException e) {
+        assertEquals("The integer for the parameter was not over 0 or -1, please retry.", e.getMessage());
+      }
+    }
+  }
 }
