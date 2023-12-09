59f2f4cc809af3afd57a1b54bbfb129e83a7911e
Adrian <131603611+derzhi@users.noreply.github.com>
2023-12-09 12:11:28 +0100

    refacored user interface name

--- src/main/java/edu/ntnu/stud/TrainDispatchApp.java
+++ src/main/java/edu/ntnu/stud/TrainDispatchApp.java
@@ -5,9 +5,9 @@ package edu.ntnu.stud;
  */
 public class TrainDispatchApp {
   public static void main(String[] args) {
-    UserInterface ui = new UserInterface();
-    ui.init();
-    ui.run();
+    UserInterface userInterface = new UserInterface();
+    userInterface.init();
+    userInterface.run();
   }
 
 
